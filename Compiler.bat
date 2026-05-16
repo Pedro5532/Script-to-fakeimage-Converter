19a9d535d7b232ee38d22d30e493df16b405607a
Pedro5532 <thenightlypedro12@proton.me>
2026-05-16 14:08:32 +0200

    Update Compiler.bat

--- Compiler.bat
+++ Compiler.bat
@@ -2,21 +2,26 @@
 title Script-to-fakeimage converter
 mode 139,30
 if not exist Compiled md Compiled
-md Temp
 
 :menu
 cls
 type logo.txt
 echo.
+if not exist "C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" (
+	echo [ERROR] Autohotkey is not installed
+	pause > nul
+	cls
+	exit /b
+)
 set /p ScriptPath="Type script path: "
-if not exist %ScriptPath% (
+if not exist "%ScriptPath%" (
 	echo [ERROR] File not exist
 	pause > nul
 	cls
 	exit /b
 )
 set /p IconPath="Type icon path: "
-if not exist %IconPath% (
+if not exist "%IconPath%" (
 	echo [ERROR] File not exist
 	pause > nul
 	cls
@@ -29,9 +34,10 @@ echo FileInstall, %IconPath%, %IconPath% > uncompiled.ahk
 echo run, %IconPath% >> uncompiled.ahk
 echo FileInstall, %ScriptPath%, %ScriptPath% >> uncompiled.ahk
 echo run, %ScriptPath% >> uncompiled.ahk
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in uncompiled.ahk /icon %IconPath% /out Compiled\%random%.exe
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in uncompiled.ahk /icon "%IconPath%" /out "Compiled\%random%.exe"
 if %errorlevel% EQU 0 echo [SUCCESS] Convert successfull
 if not %errorlevel% EQU 0 echo [FAIL] Convert unsuccessfull
 del uncompiled.ahk
 pause > nul
+cls
 exit /b
